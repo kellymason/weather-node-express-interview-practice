e5130b20413c6bf33ba169499fc9b9851422a802
Kelly Mason <mason.kelly.m@gmail.com>
2021-03-25 18:56:33 -0600

    get forecast

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 const app = require('./app')
 const port = 3001
 
+const accessKey = "6f59ecca90f84c41b3f08f63be20c53c"
+const weather = (lat, long, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=${lat},${long}&units=f`
+  request({ url, json: true }, (error, { body }) => {
+    callback(undefined, body)
+  })
+}
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`)
 })
